acada128b7241ea111dc5a1fb1d2dc57322bcdfd
Arturo Seijas <arturo.seijas@canonical.com>
2024-08-14 12:06:17 +0200

    fix integ tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,3 +11,4 @@ def pytest_addoption(parser):
         parser: Pytest parser.
     """
     parser.addoption("--charm-file", action="store")
+    parser.addoption("--wazuh-server-image", action="store")
