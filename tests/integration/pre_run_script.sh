f7a62b86f61335c8443d605a730d13f8c4a0ce15
Arturo Seijas <arturo.seijas@canonical.com>
2024-08-26 10:30:48 +0200

    fix integ tests

--- tests/integration/pre_run_script.sh
+++ tests/integration/pre_run_script.sh
@@ -9,15 +9,10 @@
 # Jenkins machine agent charm is deployed on lxd and Jenkins-k8s server charm is deployed on
 # microk8s.
 
-TESTING_MODEL="$(juju switch)"
-
 # lxd should be install and init by a previous step in integration test action.
 echo "bootstrapping lxd juju controller"
 sg snap_microk8s -c "microk8s status --wait-ready"
 sg snap_microk8s -c "juju bootstrap localhost localhost"
-
-echo "Switching to testing model"
-sg snap_microk8s -c "juju switch $TESTING_MODEL"
 
 # https://charmhub.io/opensearch/docs/t-set-up#set-parameters-on-the-host-machine
 sudo tee -a /etc/sysctl.conf > /dev/null <<EOT
