1ae20e559d1300016d3a80d7288399da253f98e5
Arturo Seijas <arturo.seijas@canonical.com>
2024-09-16 14:54:16 +0200

    fix script

--- tests/integration/pre_run_script.sh
+++ tests/integration/pre_run_script.sh
@@ -38,11 +38,10 @@ sudo sysctl -p
 # Launch a wazuh agent
 sudo snap install multipass
 multipass launch --name wazuh-agent
-multipass exec wazuh-agent -- curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | gpg --no-default-keyring --keyring gnupg-ring:/usr/share/keyrings/wazuh.gpg --import && chmod 644 /usr/share/keyrings/wazuh.gpg
-multipass exec wazuh-agent -- echo "deb [signed-by=/usr/share/keyrings/wazuh.gpg] https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
-multipass exec wazuh-agent -- apt update
-multipass exec wazuh-agent -- apt install -y gnupg apt-transport-https
-multipass exec wazuh-agent -- curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | apt-key add -
-multipass exec wazuh-agent -- WAZUH_MANAGER="10.0.0.2" apt-get install wazuh-agentecho "deb https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
-echo "$IPADDR wazuh-server.local" >> /etc/hosts
-WAZUH_MANAGER="wazuh-server.local" apt-get install wazuh-agent
+multipass exec wazuh-agent -- sudo bash -c "curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | gpg --no-default-keyring --keyring gnupg-ring:/usr/share/keyrings/wazuh.gpg --import && chmod 644 /usr/share/keyrings/wazuh.gpg"
+multipass exec wazuh-agent -- sudo bash -c "echo 'deb [signed-by=/usr/share/keyrings/wazuh.gpg] https://packages.wazuh.com/4.x/apt/ stable main' | tee -a /etc/apt/sources.list.d/wazuh.list"
+multipass exec wazuh-agent -- sudo apt update
+multipass exec wazuh-agent -- sudo apt install -y gnupg apt-transport-https
+multipass exec wazuh-agent -- sudo bash -c "curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | apt-key add -"
+multipass exec wazuh-agent -- sudo bash -c "echo $IPADDR wazuh-server.local >> /etc/hosts"
+multipass exec wazuh-agent -- sudo bash -c "WAZUH_MANAGER='wazuh-server.local' apt-get install wazuh-agent"
