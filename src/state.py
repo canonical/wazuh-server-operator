f0c596598548ddfecafacfedc22c92450ba66669
Arturo Seijas <arturo.seijas@canonical.com>
2024-09-20 11:45:59 +0200

    add proxy stuff

--- src/state.py
+++ src/state.py
@@ -5,11 +5,12 @@
 
 import json
 import logging
+import os
+import typing
 from abc import ABC, abstractmethod
-from typing import Annotated
 
 import ops
-from pydantic import BaseModel, Field, ValidationError
+from pydantic import AnyHttpUrl, BaseModel, Field, ValidationError, parse_obj_as
 
 logger = logging.getLogger(__name__)
 
@@ -26,16 +27,47 @@ class InvalidStateError(Exception):
     """Exception raised when a charm configuration is found to be invalid."""
 
 
+class ProxyConfig(BaseModel):  # pylint: disable=too-few-public-methods
+    """Configuration for accessing Synapse through proxy.
+
+    Attributes:
+        http_proxy: The http proxy URL.
+        https_proxy: The https proxy URL.
+        no_proxy: Comma separated list of hostnames to bypass proxy.
+    """
+
+    http_proxy: typing.Optional[AnyHttpUrl]
+    https_proxy: typing.Optional[AnyHttpUrl]
+    no_proxy: typing.Optional[str]
+
+
 class State(BaseModel):  # pylint: disable=too-few-public-methods
     """The Wazuh server charm state.
 
     Attributes:
         indexer_ips: list of Wazuh indexer IPs.
         certificate: the TLs certificate.
+        proxy: proxy configuration.
     """
 
-    indexer_ips: Annotated[list[str], Field(min_length=1)]
+    indexer_ips: typing.Annotated[list[str], Field(min_length=1)]
     certificate: str = Field(..., min_length=1)
+
+    @property
+    def proxy(self) -> "ProxyConfig":
+        """Get charm proxy configuration from juju charm environment.
+
+        Returns:
+            charm proxy configuration in the form of ProxyConfig.
+        """
+        http_proxy = os.environ.get("JUJU_CHARM_HTTP_PROXY")
+        https_proxy = os.environ.get("JUJU_CHARM_HTTPS_PROXY")
+        no_proxy = os.environ.get("JUJU_CHARM_NO_PROXY")
+        return ProxyConfig(
+            http_proxy=parse_obj_as(AnyHttpUrl, http_proxy) if http_proxy else None,
+            https_proxy=parse_obj_as(AnyHttpUrl, https_proxy) if https_proxy else None,
+            no_proxy=no_proxy,
+        )
 
     # pylint: disable=unused-argument
     @classmethod
