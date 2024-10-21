fc7146d2e4a8c96114e66cf508fc30cfd8b68e02
Arturo Seijas <arturo.seijas@canonical.com>
2024-10-22 00:58:19 +0200

    log stuff

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -25,6 +25,7 @@ async def test_filebeat_ok(model: Model, application: Application):
 
     Assert: the filebeat config is valid.
     """
+    assert False
     model.wait_for_idle(
         apps=[application.name], status="active", raise_on_blocked=True, timeout=1000
     )
