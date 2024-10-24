a85e8f05ffe13b84ba107d870a7f100532e0650e
arturo-seijas <102022572+arturo-seijas@users.noreply.github.com>
2024-10-24 11:09:28 +0200

    Route traffic for port 55000 (#30)

--- tests/unit/test_traefik_route_observer.py
+++ tests/unit/test_traefik_route_observer.py
@@ -66,6 +66,11 @@ def test_on_traefik_route_relation_joined_when_leader(monkeypatch: pytest.Monkey
                         "service": "juju-testing-observer-charm-service-enrole-tcp",
                         "rule": "ClientIP(`0.0.0.0/0`)",
                     },
+                    "juju-testing-observer-charm-api-tcp": {
+                        "entryPoints": ["api-tcp"],
+                        "service": "juju-testing-observer-charm-service-api-tcp",
+                        "rule": "ClientIP(`0.0.0.0/0`)",
+                    },
                 },
                 "services": {
                     "juju-testing-observer-charm-service-conn-tcp": {
@@ -74,11 +79,18 @@ def test_on_traefik_route_relation_joined_when_leader(monkeypatch: pytest.Monkey
                     "juju-testing-observer-charm-service-enrole-tcp": {
                         "loadBalancer": {"servers": [{"address": "wazuh-server.local:1515"}]}
                     },
+                    "juju-testing-observer-charm-service-api-tcp": {
+                        "loadBalancer": {"servers": [{"address": "wazuh-server.local:55000"}]}
+                    },
                 },
             },
         },
         static={
-            "entryPoints": {"conn-tcp": {"address": ":1514"}, "enrole-tcp": {"address": ":1515"}}
+            "entryPoints": {
+                "conn-tcp": {"address": ":1514"},
+                "enrole-tcp": {"address": ":1515"},
+                "api-tcp": {"address": ":55000"},
+            }
         },
     )
 
