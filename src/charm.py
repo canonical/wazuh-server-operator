497f3c09f3feaf8c4f3037e1f00b754d2d6759c8
Arturo Seijas <arturo.seijas@canonical.com>
2024-11-19 16:02:42 +0100

    fix clustering

--- src/charm.py
+++ src/charm.py
@@ -53,6 +53,7 @@ class WazuhServerCharm(CharmBaseWithState):
         self.framework.observe(self.on.wazuh_server_pebble_ready, self.reconcile)
         self.framework.observe(self.on.config_changed, self.reconcile)
         self.framework.observe(self.on[WAZUH_PEER_RELATION_NAME].relation_joined, self.reconcile)
+        self.framework.observe(self.on[WAZUH_PEER_RELATION_NAME].relation_changed, self.reconcile)
 
     def _on_install(self, _: ops.InstallEvent) -> None:
         """Install event handler."""
