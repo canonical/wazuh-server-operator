ca8e069d1c38b3236f1ba9a271e40ea13d29989d
Arturo Seijas <arturo.seijas@canonical.com>
2024-11-20 11:13:09 +0100

    Remove cat in script

--- tests/integration/pre_run_script.sh
+++ tests/integration/pre_run_script.sh
@@ -14,7 +14,6 @@ TESTING_MODEL="$(juju switch)"
 echo "bootstrapping lxd juju controller"
 # Change microk8s default file limits
 echo "ulimit -n 458752" | sudo tee -a /var/snap/microk8s/current/args/containerd-env
-sudo cat /var/snap/microk8s/current/args/containerd-env
 sudo snap restart microk8s
 sg snap_microk8s -c "microk8s status --wait-ready"
 sg snap_microk8s -c "juju bootstrap localhost localhost"
