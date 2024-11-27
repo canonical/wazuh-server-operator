b24e8fdc53dcbec6cde45ccca2838ed496023ae7
Arturo Seijas <arturo.seijas@canonical.com>
2024-11-27 13:59:37 +0100

    Check response content

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -151,6 +151,6 @@ async def application_fixture(
     )
     await model.integrate(traefik.name, application.name)
     await model.wait_for_idle(
-        apps=[application.name, traefik.name], status="active", raise_on_error=True, timeout=2000
+        apps=[application.name, traefik.name], status="active", raise_on_error=True, timeout=1500
     )
     yield application
