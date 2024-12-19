ebf7e067eee9f81af94b6511e30f3ae20d73841e
Arturo Seijas <arturo.seijas@canonical.com>
2024-12-19 17:16:11 +0100

    refactor test

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -29,39 +29,31 @@ async def test_api(model: Model, application: Application):
     Act: scale up to two units
     Assert: the default credentials are no longer valid for any of the units.
     """
-    status = await model.get_status()
-    unit = list(status.applications[application.name].units)[0]
-    address = status["applications"][application.name]["units"][unit]["address"]
-    # Check the defaults are changed instead of the new creds
-    # https://github.com/juju/python-libjuju/issues/947
-    response = requests.get(  # nosec
-        f"https://{address}:55000/security/user/authenticate",
-        auth=("wazuh", state.WAZUH_USERS["wazuh"]["default_password"]),
-        timeout=10,
-        verify=False,
-    )
-    assert response.status_code == 401
-
     await application.scale(2)
     await model.wait_for_idle(
         apps=[application.name], status="active", raise_on_blocked=True, timeout=1000
     )
 
-    unit = list(status.applications[application.name].units)[1]
-    address = status["applications"][application.name]["units"][unit]["address"]
-    response = requests.get(  # nosec
-        f"https://{address}:55000/security/user/authenticate",
-        auth=("wazuh", state.WAZUH_USERS["wazuh"]["default_password"]),
-        timeout=10,
-        verify=False,
-    )
-    assert response.status_code == 401
+    status = await model.get_status()
+    units = list(status.applications[application.name].units)
+    for unit in units:
+        address = status["applications"][application.name]["units"][unit]["address"]
+        # Check the defaults are changed instead of the new creds
+        # https://github.com/juju/python-libjuju/issues/947
+        response = requests.get(  # nosec
+            f"https://{address}:55000/security/user/authenticate",
+            auth=("wazuh", state.WAZUH_USERS["wazuh"]["default_password"]),
+            timeout=10,
+            verify=False,
+        )
+        assert response.status_code == 401
 
 
 @pytest.mark.abort_on_fail
 async def test_clustering_ok(model: Model, application: Application):
-    """Deploy the charm together with related charms and scale to two units.
-
+    """
+    Arrange: deploy the charm together with related charms.
+    Act: scale up to two units
     Assert: the clustering config is valid.
     """
     wazuh_unit = application.units[0]  # type: ignore
