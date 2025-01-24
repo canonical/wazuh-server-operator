0cc9498644f1950132467a9c8bf7db9675e947e1
Arturo Seijas <arturo.seijas@canonical.com>
2025-01-24 15:46:06 +0100

    debug

--- src/charm.py
+++ src/charm.py
@@ -143,11 +143,11 @@ class WazuhServerCharm(CharmBaseWithState):
 
         container.add_layer("wazuh", self._wazuh_pebble_layer, combine=True)
         # The prometheus exporter requires the users to be set up
+        logger.debug("Unconfigured API users %s", self.state.unconfigured_api_users)
         if not self.state.unconfigured_api_users.items():
             container.add_layer("prometheus", self._prometheus_pebble_layer, combine=True)
         container.replan()
 
-        logger.debug("Unconfigured API users %s", self.state.unconfigured_api_users)
         if self.state.unconfigured_api_users.items():
             # Current credentials that will be updated on every successful operation
             credentials = self.state.api_credentials
