a187eb8c71dee89cf485df72d5439feb13db8e9f
Arturo Seijas <arturo.seijas@canonical.com>
2025-01-31 11:21:34 +0100

    sleep

--- src/charm.py
+++ src/charm.py
@@ -273,7 +273,7 @@ class WazuhServerCharm(CharmBaseWithState):
                 "prometheus-exporter": {
                     "override": "replace",
                     "summary": "prometheus exporter",
-                    "command": "/usr/bin/python3 /srv/prometheus/prometheus_exporter.py",
+                    "command": "sleep 1; /usr/bin/python3 /srv/prometheus/prometheus_exporter.py",
                     "startup": "enabled",
                     "user": "prometheus",
                     "after": ["wazuh"],
