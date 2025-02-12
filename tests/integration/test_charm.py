81fe698b84c12ad84ecfcb63f5165cb720cff34d
Arturo Seijas <arturo.seijas@canonical.com>
2025-02-12 16:18:37 +0100

    scale up

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -27,9 +27,11 @@ APP_NAME = CHARMCRAFT["name"]
 async def test_api(model: Model, application: Application):
     """
     Arrange: deploy the charm together with related charms.
-    Act: do nothing.
+    Act: scale up to two units.
     Assert: the default credentials are no longer valid for any of the units.
     """
+    await application.scale(2)
+    await model.wait_for_idle(apps=[application.name], status="active", timeout=1400)
     status = await model.get_status()
     # Type hints are not ok here
     units = list(status.applications[application.name].units)  # type: ignore
@@ -50,9 +52,11 @@ async def test_api(model: Model, application: Application):
 async def test_clustering_ok(model: Model, application: Application):
     """
     Arrange: deploy the charm together with related charms.
-    Act: do nothing.
+    Act: scale up to two units.
     Assert: the clustering config is valid.
     """
+    await application.scale(2)
+    await model.wait_for_idle(apps=[application.name], status="active", timeout=1400)
     wazuh_unit = application.units[0]  # type: ignore
     pebble_exec = "PEBBLE_SOCKET=/charm/containers/wazuh-server/pebble.socket pebble exec"
     action = await wazuh_unit.run(
