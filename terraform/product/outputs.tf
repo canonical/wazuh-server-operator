3185a72d6a7638b78ec6b57494e6873fa0ac8676
arturo-seijas <102022572+arturo-seijas@users.noreply.github.com>
2025-04-25 13:37:13 +0200

    Add COS support for TF modules (#167)

--- terraform/product/outputs.tf
+++ terraform/product/outputs.tf
@@ -1,20 +1,60 @@
 # Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-output "app_name" {
-  description = "Name of the deployed application."
+output "wazuh_server_name" {
+  description = "Name of the deployed Wazuh server application."
   value       = module.wazuh_server.app_name
 }
 
-output "requires" {
+output "wazuh_server_requires" {
   value = {
     logging = "logging"
   }
 }
 
-output "provides" {
+output "wazuh_server_provides" {
   value = {
     grafana_dashboard = "grafana-dashboard"
     metrics_endpoint  = "metrics-endpoint"
+  }
+}
+
+output "traefik_name" {
+  description = "Name of the deployed Traefik application."
+  value       = module.traefik_k8s.app_name
+}
+
+output "traefik_requires" {
+  value = {
+    logging = "logging"
+  }
+}
+
+output "traefik_provides" {
+  value = {
+    grafana_dashboard = "grafana-dashboard"
+    metrics_endpoint  = "metrics-endpoint"
+  }
+}
+
+output "wazuh_indexer_name" {
+  description = "Name of the deployed Wazuh indexer application."
+  value       = module.wazuh_indexer.app_name
+}
+
+output "wazuh_indexer_provides" {
+  value = {
+    cos_agent = "cos-agent"
+  }
+}
+
+output "wazuh_dashboard_name" {
+  description = "Name of the deployed Wazuh dashboard application."
+  value       = module.wazuh_dashboard.app_name
+}
+
+output "wazuh_dashboard_provides" {
+  value = {
+    cos_agent = "cos-agent"
   }
 }
