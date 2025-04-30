dedaa38fef662882ba5e642e0c1c2c1ccaea7d90
Arturo Seijas <arturo.seijas@canonical.com>
2025-04-30 12:29:25 +0200

    Use tags

--- terraform/product/main.tf
+++ terraform/product/main.tf
@@ -151,7 +151,7 @@ resource "juju_integration" "wazuh_server_certificates" {
 }
 
 module "wazuh_indexer" {
-  source = "git::https://github.com/canonical/wazuh-indexer-operator//terraform/product?ref=fix-tf&depth=1"
+  source = "git::https://github.com/canonical/wazuh-indexer-operator//terraform/product?ref=rev8&depth=1"
 
   model = data.juju_model.wazuh_indexer.name
 
@@ -289,7 +289,7 @@ resource "juju_integration" "wazuh_indexer_backup" {
 }
 
 module "wazuh_dashboard" {
-  source = "git::https://github.com/canonical/wazuh-dashboard-operator//terraform/product?ref=fix-tf&depth=1"
+  source = "git::https://github.com/canonical/wazuh-dashboard-operator//terraform/product?ref=rev11&depth=1"
 
   model = data.juju_model.wazuh_dashboard.name
 
