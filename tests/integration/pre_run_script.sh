0a8c7e5a702db465a058ff3e82777a777d81c08f
swetha1654 <swetha.swaminathan@canonical.com>
2025-06-18 11:12:11 +0530

    Change pre-run-script

--- tests/integration/pre_run_script.sh
+++ tests/integration/pre_run_script.sh
@@ -15,18 +15,13 @@ TESTING_MODEL="$(juju switch)"
 # lxd should be install and init by a previous step in integration test action.
 echo "bootstrapping lxd juju controller"
 # Change microk8s default file limits
-echo "ulimit -n 458752" | sudo tee -a /var/snap/microk8s/current/args/containerd-env
-sudo snap restart microk8s
-sg snap_microk8s -c "microk8s status --wait-ready"
-sg snap_microk8s -c "juju bootstrap localhost localhost"
+echo "ulimit -n 458752" | sudo tee -a /var/snap/k8s/current/args/containerd-env
+sudo snap restart k8s
+sudo k8s status --wait-ready --timeout 5m
+sudo juju bootstrap localhost localhost
 
 echo "Switching to testing model"
-sg snap_microk8s -c "juju switch $TESTING_MODEL"
-
-
-IPADDR=$(ip -4 -j route get 2.2.2.2 | jq -r '.[] | .prefsrc')
-sudo microk8s enable "metallb:$IPADDR-$IPADDR"
-sudo microk8s status
+juju switch $TESTING_MODEL
 
 # https://charmhub.io/opensearch/docs/t-set-up#set-parameters-on-the-host-machine
 sudo tee -a /etc/sysctl.conf > /dev/null <<EOT
