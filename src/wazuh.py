0b4719b8691db7a247c9ef6ad485c5f5743f719d
swetha1654 <swetha.swaminathan@canonical.com>
2025-06-23 12:49:57 +0530

    lint issue

--- src/wazuh.py
+++ src/wazuh.py
@@ -58,7 +58,7 @@ logger = logging.getLogger(__name__)
 
 
 class OpenCTIIntegrationMissingError(Exception):
-    """OpenCTI integration is missing."""
+    """OpenCTI integration is missing but required."""
 
 
 class WazuhInstallationError(Exception):
@@ -123,6 +123,7 @@ def _update_wazuh_configuration(  # pylint: disable=too-many-locals, too-many-ar
         opencti_url: OpenCTI URL.
 
     Raises:
+        OpenCTIIntegrationMissingError: if the OpenCTI integration is missing but required.
         WazuhConfigurationError: if the configuration is invalid or missing required elements.
     """
     ossec_config = container.pull(OSSEC_CONF_PATH, encoding="utf-8").read()
