b547094506028592dfd01c1f725967686923ec6c
Sebastien Georget <sebastien.georget@canonical.com>
2025-06-24 11:17:55 +0200

    Use last indexer terraform release

--- terraform/product/main.tf
+++ terraform/product/main.tf
@@ -151,7 +151,7 @@ resource "juju_integration" "wazuh_server_certificates" {
 }
 
 module "wazuh_indexer" {
-  source = "git::https://github.com/canonical/wazuh-indexer-operator//terraform/product?ref=rev9&depth=1"
+  source = "git::https://github.com/canonical/wazuh-indexer-operator//terraform/product?ref=rev10&depth=1"
 
   model = data.juju_model.wazuh_indexer.name
 
