580713fc9c0eeb9747c27fb4d5a39da71b4cd9f9
Arturo Seijas <arturo.seijas@canonical.com>
2025-09-19 17:20:45 +0200

    Fix fixes

--- src/charm.py
+++ src/charm.py
@@ -81,7 +81,7 @@ class WazuhServerCharm(CharmBaseWithState):
         Returns: a list of FQDNs.
         """
         peer_relation = self.model.get_relation(WAZUH_PEER_RELATION_NAME)
-        if not peer_relation and not peer_relation.units:
+        if not peer_relation or not peer_relation.units:
             return [getfqdn()]
         logger.error("UNITSSSS: %s", peer_relation.units)
         return [
