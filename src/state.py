faecf3e5e81b091fe345813cda61b0522b18264b
niwamo <nm@niwamo.com>
2025-11-13 19:44:44 -0600

    abbreviate previously extensive CSR logging

--- src/state.py
+++ src/state.py
@@ -148,16 +148,23 @@ def _fetch_matching_certificates(
     Returns:
         the certificates matching the CSR.
     """
-    logger.debug(
-        "Matching CSR %s for certificates %s", certificate_signing_request, provider_certificates
-    )
-    return [
+    csr_to_match = certificate_signing_request.replace("\n", "")
+    matching_certificates = [
         certificate
         for certificate in provider_certificates
-        if certificate.csr.replace("\n", "") == certificate_signing_request.replace("\n", "")
+        if certificate.csr.replace("\n", "") == csr_to_match
         and not certificate.revoked
     ]
 
+    abbreviated_csr = f"{csr_to_match[:80]}..."
+    if not matching_certificates:
+        logger.debug(
+            "Could not find matching certificate for CSR %s", abbreviated_csr
+        )
+    else:
+        logger.debug("Found matching certificate for CSR %s", abbreviated_csr)
+
+    return matching_certificates
 
 def _fetch_ssh_repository_key(model: ops.Model, config: WazuhConfig) -> str | None:
     """Fetch the SSH key for the repository.
