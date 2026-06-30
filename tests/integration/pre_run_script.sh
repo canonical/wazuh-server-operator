78e804305ff852dbe11ad8f5d3b2c7dbff04c1f4
javierdelapuente <javier.delapuente@canonical.com>
2026-06-30 13:37:44 +0200

    chore: migrate CI to charm-ci / opcli (#403)

--- tests/integration/pre_run_script.sh
+++ tests/integration/pre_run_script.sh
@@ -3,29 +3,14 @@
 # Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-# Pre-run script for integration test operator-workflows action.
-# https://github.com/canonical/operator-workflows/blob/main/.github/workflows/integration_test.yaml
-
+# Pre-run script for integration tests.
 # OpenSearch charms are deployed on lxd and Wazuh Server charm is deployed on Canonical K8S.
 
 set -euo pipefail
 
-TESTING_MODEL="$(juju switch)"
-
-# lxd should be install and init by a previous step in integration test action.
-echo "bootstrapping lxd juju controller"
-juju bootstrap localhost localhost
-
-echo "Switching to testing model"
-juju switch "$TESTING_MODEL"
-
 # https://charmhub.io/opensearch/docs/t-set-up#set-parameters-on-the-host-machine
-sudo tee -a /etc/sysctl.conf > /dev/null <<EOT
-vm.max_map_count=262144
-vm.swappiness=0
-net.ipv4.tcp_retries2=5
-fs.file-max=1048576
-EOT
-
-sudo sysctl -p
-
+# Applied temporarily — no reboot expected.
+sudo sysctl -w vm.max_map_count=262144
+sudo sysctl -w vm.swappiness=0
+sudo sysctl -w net.ipv4.tcp_retries2=5
+sudo sysctl -w fs.file-max=1048576
